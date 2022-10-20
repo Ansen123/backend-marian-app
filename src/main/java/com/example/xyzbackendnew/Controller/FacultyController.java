b54dbb469430e7f4c58d73dee31476bf527d32ba
Ansen123 <ansenfp@gmail.com>
2022-10-21 00:23:30 +0530

    first commit

--- src/main/java/com/example/xyzbackendnew/Controller/FacultyController.java
+++ src/main/java/com/example/xyzbackendnew/Controller/FacultyController.java
@@ -1,11 +1,11 @@
 package com.example.xyzbackendnew.Controller;
 
 import com.example.xyzbackendnew.Model.FacultyModel;
-import com.example.xyzbackendnew.Model.StudentModel;
 import com.example.xyzbackendnew.dao.FacultyDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -25,5 +25,18 @@ public class FacultyController {
     @GetMapping("/viewF")
     public List<FacultyModel> view() {
         return (List<FacultyModel>) dao.findAll();
+    }
+
+    @CrossOrigin(origins = "*")
+    @Transactional
+    @PostMapping(path = "/deleteF",consumes = "application/json", produces = "application/json")
+    public String deleteFaculty(@RequestBody FacultyModel faculty) {
+        dao.deleteFacultyById(faculty.getId());
+        return "{status:'Success'}";
+    }
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/searchF",consumes = "application/json",produces = "application/json")
+    public List<FacultyModel>searchFaculty(@RequestBody FacultyModel faculty){
+        return (List<FacultyModel>) dao.searchFaculty(faculty.getName());
     }
 }
--- src/main/java/com/example/xyzbackendnew/dao/FacultyDao.java
+++ src/main/java/com/example/xyzbackendnew/dao/FacultyDao.java
@@ -1,8 +1,20 @@
 package com.example.xyzbackendnew.dao;
 
 import com.example.xyzbackendnew.Model.FacultyModel;
-import com.example.xyzbackendnew.Model.StudentModel;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface FacultyDao extends CrudRepository<FacultyModel,Integer> {
+    @Modifying
+    @Query(value = "DELETE FROM `faculty` WHERE `id`= :id",nativeQuery = true)
+    void deleteFacultyById(Integer id);
+
+    @Query(value ="SELECT `id`, `joining`, `address`, `d_name`, `des_name`, `dob`, `education`, `mob`, `name` FROM `faculty` WHERE name =:name",nativeQuery = true)
+    List<FacultyModel> searchFaculty(String name);
 }
+
+
